8c0f50fcbdd1678e73d1e3e369544bfd5837813c
Crazykwak <kwj1830@naver.com>
2022-03-05 14:09:34 +0900

    Appconfig for DIP

--- src/test/java/hello/core/order/OrderServiceTest.java
+++ src/test/java/hello/core/order/OrderServiceTest.java
@@ -1,16 +1,25 @@
 package hello.core.order;
 
+import hello.core.AppConfig;
 import hello.core.member.Grade;
 import hello.core.member.Member;
 import hello.core.member.MemberService;
 import hello.core.member.MemberServiceImpl;
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class OrderServiceTest {
 
-    MemberService memberService = new MemberServiceImpl();
-    OrderService orderService = new OrderServiceImpl();
+    MemberService memberService;
+    OrderService orderService;
+
+    @BeforeEach
+    public void beforeEach(){
+        AppConfig appconfig = new AppConfig();
+        memberService = appconfig.memberService();
+        orderService = appconfig.orderService();
+    }
 
     @Test
     void createOrder() {
@@ -20,7 +29,7 @@ public class OrderServiceTest {
 
         //when
         memberService.join(member);
-        Order order = new Order(memberId, "doll", 1000, 1000);
+        Order order = orderService.createOrder(memberId,"doll",20000);
 
         //then
         Assertions.assertThat(order.getDiscountPrice()).isEqualTo(1000);
