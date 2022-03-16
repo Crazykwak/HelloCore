a316cdecfc51782ada0c1f3207d9eb0761d71a78
Crazykwak <kwj1830@naver.com>
2022-03-16 21:14:26 +0900

    Bean PreConstruct PreDestroy

--- src/test/java/hello/core/lifecycle/NetworkClient.java
+++ src/test/java/hello/core/lifecycle/NetworkClient.java
@@ -1,14 +1,14 @@
 package hello.core.lifecycle;
 
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
 public class NetworkClient {
 
     private String url;
 
     public NetworkClient(){
         System.out.println("생성자 호출 url = " + url);
-        connect();
-        call("초기화 연결 메시지");
-
     }
 
     public void setUrl(String url) {
@@ -22,11 +22,22 @@ public class NetworkClient {
     }
 
     public void call(String message) {
-        System.out.println("call : " + url + "message = " + message);
+        System.out.println("call : " + url + " message = " + message);
     }
 
     //서비스 종류시 호출
     public void disconnect(){
         System.out.println("close : " + url );
+    }
+
+    @PostConstruct
+    public void init(){
+        connect();
+        call("초기화 연결 메시지");
+    }
+
+    @PreDestroy
+    public void close(){
+        disconnect();
     }
 }
